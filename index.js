b8c0583a17de3f5815b04d0af528d12617c28308
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-08-02 19:46:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
